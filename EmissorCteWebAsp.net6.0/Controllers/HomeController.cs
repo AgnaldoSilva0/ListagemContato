b9eb0af748599ce1cac8fa44ccf53ce702665d59
Agnaldo Silva <agnaldo.santos24@hotmail.com>
2022-07-08 12:39:35 -0300

    Adicionar arquivos de projeto.

---
+++ EmissorCteWebAsp.net6.0/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using EmissorCteWebAsp.net6._0.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EmissorCteWebAsp.net6._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.Nome = "Visitante";
+            home.Email = "Vazio";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
